96a32723de99d2c8cd82753c2e7102e1cdd1b78d
Xedice Mustafayeva <khadijamm@code.edu.az>
2023-08-07 05:29:48 +0400

    First Commit

---
+++ C#HW7/Extensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_HW7
+{
+    internal static class Extensions
+    {
+        //public static bool CustomContains(this string input, string search)
+        //{
+        //    string check = input.ToLower();
+        //    search = search.ToLower();
+        //    char[] letters = check.ToCharArray();
+            
+
+        //    int wordLength = search.Length;
+        //    bool output = false;
+
+        //    for (int i = 0; i < letters.Length; i++)
+        //    {
+        //        if (letters[i] == search[0])
+        //        {
+        //            string result = new string(letters, i, wordLength);
+        //            if (result == search)
+        //            {
+        //                output = true;
+        //                break;
+                        
+        //            }
+        //        }
+        //    }
+        //    return output;
+
+
+        //}
+
+
+        public static void CustomSplit(this string sentence,char input)
+        {
+            char[]letter=sentence.ToCharArray();
+            char[] tempLetter = new char[0];
+            string[]result = new string[0];
+            
+
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (letter[i] != input)
+                {
+                    Array.Resize(ref tempLetter,tempLetter.Length+1);
+                    tempLetter[tempLetter.Length-1] = letter[i];
+                }
+                else
+                {
+                    string word = new string(tempLetter);
+                    Array.Resize(ref result,result.Length+1);
+                    result[result.Length-1] = word;
+                    tempLetter = new char[0];
+                }
+            }
+            if (tempLetter.Length > 0)
+            {
+                string word = new string(tempLetter);
+                Array.Resize(ref result, result.Length + 1);
+                result[result.Length - 1] = word;
+            }
+
+
+            foreach (string output in result)
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ C#HW7/Program.cs
@@ -0,0 +1,20 @@
+﻿using C_HW7;
+
+//Console.WriteLine("Enter your sentence");
+//string input = Console.ReadLine();
+
+
+//Console.WriteLine("Enter your search");
+//string search = Console.ReadLine();
+
+
+//Console.WriteLine(input.CustomContains(search));
+
+Console.WriteLine("Enter your sentence");
+string sentence=Console.ReadLine();
+
+Console.WriteLine("Enter your split character");
+char input=char.Parse(Console.ReadLine());
+
+
+sentence.CustomSplit(input);
